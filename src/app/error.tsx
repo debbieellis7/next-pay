7e57bce17be8570793284fdd0bca6326c80c0139
debbieellis7 <1debbieellis@gmail.com>
2024-10-22 22:52:15 +0800

    Add Error page

---
+++ src/app/error.tsx
@@ -0,0 +1,6 @@
+"use client";
+import NextError from "next/error";
+
+export default function ErrorPage({ error }: { error: Error }) {
+  return <NextError statusCode={500} title={error.message} />;
+}
