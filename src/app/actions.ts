94bbdc6f0f7ce279c1b8e5162f60ef08f78ce5a6
debbieellis7 <1debbieellis@gmail.com>
2024-10-22 20:25:37 +0800

    Prevent multiple form submissions and redirect to invoice URL with ID after form submission

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import { redirect } from "next/navigation";
 import { Invoices } from "@/db/schema";
 import { db } from "@/db";
 
@@ -6,9 +7,16 @@ export const createAction = async (formData: FormData) => {
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   const description = formData.get("description") as string;
 
-  await db.insert(Invoices).values({
-    value,
-    description,
-    status: "open",
-  });
+  const results = await db
+    .insert(Invoices)
+    .values({
+      value,
+      description,
+      status: "open",
+    })
+    .returning({
+      id: Invoices.id,
+    });
+
+  redirect(`/invoices/${results[0].id}`);
 };
--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,16 +1,36 @@
+"use client";
+import { SyntheticEvent, useState } from "react";
+import Form from "next/form";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import SubmitButton from "@/components/SubmitButton";
 import { createAction } from "@/app/actions";
 
-const NewInvoicePage = async () => {
+const NewInvoicePage = () => {
+  const [pending, setPending] = useState(false);
+
+  // Prevent multiple form submission
+  const handleOnSubmit = async (e: SyntheticEvent) => {
+    if (pending) {
+      e.preventDefault();
+      return;
+    }
+
+    setPending(true);
+  };
+
   return (
     <main className="flex flex-col justify-center h-full gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold">Create Invoice</h1>
       </div>
-      <form action={createAction} className="grid gap-4 max-w-xs">
+      <Form
+        action={createAction}
+        onSubmit={handleOnSubmit}
+        className="grid gap-4 max-w-xs"
+      >
         <div>
           <Label htmlFor="name" className="block font-semibold text-sm mb-2">
             Billing Name
@@ -39,9 +59,9 @@ const NewInvoicePage = async () => {
           <Textarea id="description" name="description"></Textarea>
         </div>
         <div>
-          <Button className="w-full font-semibold">Submit</Button>
+          <SubmitButton />
         </div>
-      </form>
+      </Form>
     </main>
   );
 };
