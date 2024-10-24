dae5a6b77595e67749d71449e189b3131e4caa55
debbieellis7 <1debbieellis@gmail.com>
2024-10-24 09:02:03 +0800

    Resolve Clerk authentication error by updating to version 6.0.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col justify-center h-screen text-center gap-6 max-w-5xl mx-auto">
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">NextPay</h1>
       <p>
         <Button asChild>
