0fbab279a26f76ee6802cbbccdb098b920621b52
debbieellis7 <1debbieellis@gmail.com>
2024-11-11 01:25:54 +0800

    Resolve bug on sign-in and sign-up pages when revisiting exact URL after successful process

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,25 +1,19 @@
+"use client";
+
 // External dependencies
-import { auth } from "@clerk/nextjs/server";
-import { SignIn } from "@clerk/nextjs";
+import { useUser, SignIn } from "@clerk/nextjs";
 
 // Internal components
 import AuthSpinner from "@/components/AuthSpinner";
 
-export default async function SignInPage() {
-  const { userId } = await auth();
+export default function SignInPage() {
+  const { isLoaded } = useUser();
 
   return (
     <main className="flex justify-center items-start mt-16">
       <div className="max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:pt-28">
-        {userId ? (
-          <AuthSpinner />
-        ) : (
-          <SignIn
-            fallbackRedirectUrl={
-              process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL
-            }
-          />
-        )}
+        {!isLoaded && <AuthSpinner />}
+        <SignIn />
       </div>
     </main>
   );
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,25 +1,19 @@
+"use client";
+
 // External dependencies
-import { auth } from "@clerk/nextjs/server";
-import { SignUp } from "@clerk/nextjs";
+import { useUser, SignUp } from "@clerk/nextjs";
 
 // Internal components
 import AuthSpinner from "@/components/AuthSpinner";
 
-export default async function SignUpPage() {
-  const { userId } = await auth();
+export default function SignUpPage() {
+  const { isLoaded } = useUser();
 
   return (
     <main className="flex justify-center items-start mt-16">
       <div className="max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:pt-28">
-        {userId ? (
-          <AuthSpinner />
-        ) : (
-          <SignUp
-            fallbackRedirectUrl={
-              process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL
-            }
-          />
-        )}
+        {!isLoaded && <AuthSpinner />}
+        <SignUp />
       </div>
     </main>
   );
